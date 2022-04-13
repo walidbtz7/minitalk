218beb41aa8ac39b2a396176d3a6b2b7e12b3d44
Walid Boutzougarte <wboutzou@e2r6p4.1337.ma>
2022-04-13 07:10:38 +0000

    add signal handler

--- client.c
+++ client.c
@@ -1,6 +1,20 @@
 #include "minitalk.h"
 
+int convert(int n)
+{
+    int b = 0;
+    while(n >= 1)
+    {
+        b = (n % 2);
+        ft_printf("%d",n % 2);
+        n = n / 2;
+    }
+    return (b);
+}
 int main(int argc,char **argv)
 {
-    kill(ft_atoi(argv[1]),SIGKILL);
+    if(argc == 3)
+        return (0);
+    
+    kill(ft_atoi(argv[1]),SIGUSR2);
 }
--- server.c
+++ server.c
@@ -1,10 +1,19 @@
 #include "minitalk.h"
 
+void sig_handler(int signum){
+    if(signum == SIGUSR1)
+        ft_printf("labas \n");
+    else
+        ft_printf("salam \n");
+}
 int main()
 {
-        ft_printf("%d\n",getpid());
-     while (1)
-    {
+    ft_printf("%d\n",getpid());
+    signal(SIGUSR2,sig_handler); // Register signal handler
+    signal(SIGUSR1,sig_handler); // Register signal handler
+    for(int i=1;;i++)
+    {    //Infinite loop
+        sleep(1);  // Delay for 1 second
     }
-    
+    return 0;
 }
