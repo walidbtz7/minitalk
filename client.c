86f2f9f96c7b1684eff9b7edc4d6824dcdd361af
Walid Boutzougarte <wboutzou@e2r9p4.1337.ma>
2022-04-14 05:58:01 +0000

    finish

--- client.c
+++ client.c
@@ -9,17 +9,18 @@ int main(int argc,char **argv)
     pid = ft_atoi(argv[1]);
     i = 0;
     if(argc == 3)
-        return (0);
+        
     while (argv[2][i] != '\0')
     {
         j = 7;
         while (j >= 0)
         {
-             if(argv[2][i] &  1 >> j)
+             if(argv[2][i] &  1 << j)
                 kill(pid,SIGUSR1);
              else
                 kill(pid,SIGUSR2);
             j--;
+            usleep(200);
         }
         i++;
     }
--- server.c
+++ server.c
@@ -11,48 +11,29 @@ int ft_pow(int pow)
     }
     return (n);
 }
+
 void sig_handler(int signum){
     static int i = 7;
     static int n = 0;
-    while (i >= 0)
-    {
-         if(signum == SIGUSR1)
-            n += 1 * ft_pow(i);
-        i--;
-    }
+
+    if(signum == SIGUSR1)
+        n += 1 * ft_pow(i);
+    i--;
+       
     if(i == -1)
     {
         i = 7;
+        ft_printf("%c",n);
         n = 0;
     }
 }
-// void convert(char *str)
-// {
-//     int i;
-//     int n = 0;
-//     int x = 0;
-//     while (str[x] != '\0')
-//     {
-//         i = 7;
-//         n = 0;
-//         while (i  >= 0)
-//         {
-//             if(str[x] == '1')
-//                 n += 1 * ft_pow(i);
-//             i--;
-//             x++;
-//         }
-//         ft_printf("%c",n);
-//     }
-// }
+
 int main()
 {
     ft_printf("%d\n",getpid());
     signal(SIGUSR2,sig_handler); 
     signal(SIGUSR1,sig_handler); 
     while (1)
-    {
-
-    }
+        pause();
     return 0;
 }
